189f12fe2d52f01fa5655f99805184491881a8c8
chechna9 <ja_bouchouareb@esi.dz>
2022-03-25 10:02:41 +0100

    before pull

--- iwd2022/lib/main.dart
+++ iwd2022/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:iwd2022/screens/needs.dart';
 import 'package:iwd2022/screens/showRecettes.dart';
+import 'package:iwd2022/screens/test_swiper.dart';
 
 void main() async {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(),
-    initialRoute: '/needs', // initialisation de la route
+    initialRoute: '/testSwiper', // initialisation de la route
     routes: {
       // '/showRecette': (context) => showRecettes(),
       '/needs': (context) => Needs(),
+      '/testSwiper': (context) => TestSwiper(),
     },
   ));
 }
---
+++ iwd2022/lib/screens/test_swiper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TestSwiper extends StatefulWidget {
+  const TestSwiper({Key? key}) : super(key: key);
+
+  @override
+  State<TestSwiper> createState() => _TestSwiperState();
+}
+
+class _TestSwiperState extends State<TestSwiper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // body: ,
+        );
+  }
+}
