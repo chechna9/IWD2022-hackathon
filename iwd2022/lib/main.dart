668e66e3a0e700c28caa2ffbf425ce4e6f2893c5
mohamedaminebentayeb <jm_bentayeb@esi.dz>
2022-03-25 10:00:32 +0100

    menu page in progres

--- iwd2022/lib/main.dart
+++ iwd2022/lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:iwd2022/screens/menu.dart';
 import 'package:iwd2022/screens/showRecettes.dart';
+import 'package:iwd2022/screens/statistics.dart';
 void main()async  {
  
    
  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
-      
+      fontFamily: "Roboto",
 
     ),
-    initialRoute: '/showRecette', // initialisation de la route
+    initialRoute: '/menu', // initialisation de la route
     routes: {
 '/showRecette': (context) =>
            showRecettes(),  
-     
-
+'/statistics': (context) =>
+           Statistics(),
+'/menu': (context) =>
+           MenuList(),
     },
   ));}
