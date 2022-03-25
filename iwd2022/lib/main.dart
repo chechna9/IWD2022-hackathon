3141e0ac3b8295d7ed7cb637a8dead8967e6fb97
chamsou-Br <jc_berkane@esi.dz>
2022-03-25 12:44:57 +0100

    'chamsou'

--- iwd2022/lib/main.dart
+++ iwd2022/lib/main.dart
@@ -1,43 +1,48 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
+import 'package:iwd2022/screens/Drawer.dart';
 import 'package:iwd2022/screens/Home.dart';
+import 'package:iwd2022/screens/LetsCookies.dart';
+void main() {
+  runApp(const MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-import 'package:iwd2022/screens/menu.dart';
-import 'package:iwd2022/screens/needs.dart';
-import 'package:iwd2022/screens/showRecettes.dart';
-import 'package:iwd2022/screens/statistics.dart';
-void main()async  {
- 
-   
- runApp(MaterialApp(
-     title: 'Flutter Demo',
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-    debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
-    initialRoute: '/home', // initialisation de la route
-    routes: {
-                 '/home' : (context) => MyHome(),
-'/showRecette': (context) =>
-           MyHome(),  
-'/statistics': (context) =>
-           MyHome(),
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
-'/menu': (context) =>
-           MyHome(),
-      '/needs': (context) => const MyHome()
-    }));}
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
       
-
+      appBar: AppBar(
+        title: Text("Flutter Learning"),
+  ),
+      body:Center(
+        child: MyHome()
+      ),
+  drawer : MyDrawer()
+    );
+  }
+}
--- iwd2022/lib/screens/Home.dart
+++ iwd2022/lib/screens/Home.dart
@@ -14,25 +14,58 @@ class _MyHomeState extends State<MyHome> {
       decoration: BoxDecoration(color: Color(0xff6E928E)),
       child: Column(
         children: [
-            Text("BienVenue dans votre refregirateur"),
+          SizedBox(height: 40,),
+          Padding(padding: EdgeInsets.only(left: 10 , right: 10),child:  Text("BienVenue dans votre refregirateur",textAlign: TextAlign.center,style: TextStyle(color: Colors.white,fontWeight:  FontWeight.bold , fontSize:  22),),),
+            SizedBox(height: 50,),
             Column(
               children: [
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Container(
-                      padding: EdgeInsets.all(30),
-                      decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10),border: Border.all(width: 2,color: Color(0xFFE5E5E5)),),
+                    padding: EdgeInsets.symmetric(vertical: 30 , horizontal: 0),
+                      width: 130,
+                      decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(15),border: Border.all(width: 2,color: Color(0xFFE5E5E5)),),
                       child: Column(children: [
-                        Icon(Icons.add , size: 24,color: Colors.black,),
+                        Icon(Icons.production_quantity_limits , size: 34,color : Color(0XFF63BDCC)),
+                        Text("Products",style: TextStyle(fontWeight: FontWeight.bold , fontSize: 22 , color: Colors.black),)
+                      ]),
+                    ),
+                    SizedBox(width: 0,),
+                    Container(
+                      width: 130,
+                      padding: EdgeInsets.symmetric(vertical: 30 , horizontal: 0),
+                      decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(15),border: Border.all(width: 2,color: Color(0xFFE5E5E5)),),
+                      child: Column(children: [
+                        Icon(Icons.receipt , size: 34,color: Color(0XFF63BDCC),),
+                        Text("Recips",style: TextStyle(fontWeight: FontWeight.bold , fontSize: 22 , color: Colors.black),)
+                      ]),
+                    )
+                  ],
+                ),
+                SizedBox(height : 35),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 130,
+                      padding: EdgeInsets.symmetric(vertical: 30 , horizontal: 0),
+                      decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(15),border: Border.all(width: 2,color: Color(0xFFE5E5E5)),),
+                      child: Column(children: [
+                        Icon(Icons.list , size: 34,color : Color(0XFF63BDCC),),
                         Text("Needs",style: TextStyle(fontWeight: FontWeight.bold , fontSize: 22 , color: Colors.black),)
                       ]),
                     ),
+                    SizedBox(width: 0,),
                     Container(
-                      padding: EdgeInsets.all(30),
-                      decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10),border: Border.all(width: 2,color: Color(0xFFE5E5E5)),),
+                      width: 130,
+                      padding: EdgeInsets.symmetric(vertical: 30 , horizontal: 0),
+                      decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(15),border: Border.all(width: 2,color: Color(0xFFE5E5E5)),),
                       child: Column(children: [
-                        Icon(Icons.add , size: 24,color: Colors.black,),
-                        Text("Needs",style: TextStyle(fontWeight: FontWeight.bold , fontSize: 22 , color: Colors.black),)
+                        Icon(Icons.science , size: 34,color : Color(0XFF63BDCC)),
+                        Text("Statistics",style: TextStyle(fontWeight: FontWeight.bold , fontSize: 22 , color: Colors.black),)
                       ]),
                     )
                   ],
